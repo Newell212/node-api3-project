2025c73e38ba8623face53ab42697c28ab4c4d66
Michael Newell <michael.newell212@gmail.com>
2023-06-22 12:29:10 -0600

    WIP

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -13,7 +13,7 @@ async function validateUserId(req, res, next) {
     const user = await User.getById(req.params.id)
     if(!user) {
       res.status(404).json({
-        message: 'no such user',
+        message: 'user not found',
       })
     } else {
       req.user = user
@@ -28,7 +28,15 @@ async function validateUserId(req, res, next) {
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  const { name } = req.body
+  if(!name || !name.time()) {
+    res.status(404).json({
+      message: "missing required name field"
+    })
+  } else {
+    req.name = name.trim()
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
